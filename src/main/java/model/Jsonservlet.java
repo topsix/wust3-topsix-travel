31af0035e9d6760d9c857fc054dc125e332e4a0d
ccf123 <605642501@qq.com>
2017-07-07 18:36:01 +0800

    add

---
+++ src/main/java/model/Jsonservlet.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Jsonservlet<T> {
+   String status;
+   String message;
+   String data;
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+public String getData() {
+	return data;
+}
+public void setData(String data) {
+	this.data = data;
+}
+   
+   
+}
+
