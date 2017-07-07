40321dde0f6f03674e6697955652fd5dbedd8fcb
tutimesong <1059392389@qq.com>
2017-07-07 19:35:44 +0800

    add

--- src/main/java/Servlets/Regservlet.java
+++ src/main/java/Servlets/Regservlet.java
@@ -8,62 +8,52 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import model.Jsonservlet;
+
 public class Regservlet extends HttpServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String username=request.getParameter("username");
+		Jsonservlet<String> json=new Jsonservlet<String>();
+		
+	
+    	response.setContentType("text/json");
+    	Gson gson=new GsonBuilder().create();
+    	if(username.equals("Bill"))
+    	{
+    		json.setStatus("-1");
+    		json.setMessage("username exits");
+    		json.setData("error");
+    		String result=gson.toJson(json);
+        	
+    		response.getWriter().append(result); 
+    	}
+    	
+    	else
+    	{
+    		json.setStatus("0");
+    		json.setMessage("username success");
+    		json.setData("ok");
+            String result=gson.toJson(json);
+        	
+    		response.getWriter().append(result); 
+    	}
+    	
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
+	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the POST method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		this.doGet(request, response);
+		
 	}
 
 }
