a175dbadcfbb82ad0c3b2cfdab935132fe7efd0b
tutimesong <1059392389@qq.com>
2017-07-13 09:53:52 +0800

    add

---
+++ src/main/java/Filter/Logfilter.java
@@ -0,0 +1,43 @@
+package Filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class Logfilter implements Filter {
+
+ 
+    public Logfilter() {
+        // TODO Auto-generated constructor stub
+    }
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		if (request.getSession().getAttribute("username") == null) {
+			response.sendRedirect(request.getContextPath() + "/login.htm");
+		} else {
+			arg2.doFilter(arg0, arg1);
+		}
+
+	}
+
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
