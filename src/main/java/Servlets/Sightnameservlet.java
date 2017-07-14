337c78cd8c444f639a09a68a6a07c409b928d48f
tutimesong <1059392389@qq.com>
2017-07-14 10:46:01 +0800

    add

--- src/main/java/Servlets/Sightnameservlet.java
+++ src/main/java/Servlets/Sightnameservlet.java
@@ -17,7 +17,7 @@ public class Sightnameservlet extends HttpServlet {
          if(sightname!=null)
          {
         	 request.getSession().setAttribute("sightname", sightname);
-        	 response.sendRedirect(request.getContextPath() + "/pages/ticket1.html");
+        	 response.sendRedirect(request.getContextPath() + "/pages/ticket.html");
          }
 		
 	}
---
+++ src/main/webapp/js/order.js
@@ -0,0 +1,54 @@
+function commit()
+{
+	var price=document.getElementById("price").innerHTML; 
+     var sightname=document.getElementById("sightname").innerHTML;
+     var status="unfinish";
+	$.ajax({  
+        type: "POST",  
+        url: "../servlet/Addorderservlet?sightname="+sightname+"&price="+price+"&status="+status,  
+        dataType:"json",
+       // data: $('#login_form').serialize(), 
+         
+          
+       
+         success: function(data){    
+        	
+        	//var obj = JSON.parse(data); //由JSON字符串转换为JSON对象
+        	
+           if(data.status=="1")
+        	   alert("订单生成成功");
+        	 
+          else
+        	  alert("订单生成失败");
+            	 
+         }  
+    })  ;    
+
+}
+function buy()
+{
+	var price=document.getElementById("price").innerHTML; 
+     var sightname=document.getElementById("sightname").innerHTML;
+     var status="finish";
+	$.ajax({  
+        type: "POST",  
+        url: "../servlet/Addorderservlet?sightname="+sightname+"&price="+price+"&status="+status,  
+        dataType:"json",
+       // data: $('#login_form').serialize(), 
+         
+          
+       
+         success: function(data){    
+        	
+        	//var obj = JSON.parse(data); //由JSON字符串转换为JSON对象
+        	
+           if(data.status=="1")
+        	   alert("购买成功");
+        	 
+          else
+        	  alert("购买失败生成失败");
+            	 
+         }  
+    })  ;    
+
+}
